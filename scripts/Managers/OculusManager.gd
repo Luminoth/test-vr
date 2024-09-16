15a132e142fc9ed9b92ef754ac96415a1a498766
Shane Lillie <ignignokterr@gmail.com>
2024-09-16 15:17:36 -0700

    Do a little more while we're here

--- scripts/Managers/OculusManager.gd
+++ scripts/Managers/OculusManager.gd
@@ -1,14 +1,41 @@
 extends Node
 
 var oculus_initialized = false
+@export var app_id = ""
 
 func _ready():
     # PC not supported by the Platform SDK plugin
     if OS.get_name() != "android":
         return
 
-    GDOculusPlatform.initialize_android_async("12345") \
+    GDOculusPlatform.initialize_android_async(app_id)\
     .then(func(_initialization_resp):
-        # TODO
-        print("Oculus: " + _initialization_resp)
+        print("Oculus Platform initialized!")
+
+        # Is the user entitled to this app?
+        GDOculusPlatform.user_get_is_viewer_entitled()\
+        .then(func(_is_viewer_entitled_resp):
+            print("User is entitled!")
+        )\
+        .error(func(is_viewer_entitled_err):
+            print("User not entitled / error! ", is_viewer_entitled_err)
+        )
+
+        GDOculusPlatform.user_get_logged_in_user()\
+        .then(func(resp):
+            # TODO: store this stuff
+            print("LOGGED IN USER INFO:")
+            print("--------------------")
+
+            print("ID: ", resp.id)
+            print("OCULUS ID: ", resp.oculus_id)
+            print("DISPLAY NAME: ", resp.display_name)
+            print("IMAGE URL: ", resp.image_url)
+            print("SMALL IMAGE URL: ", resp.small_image_url)
+        ).error(func(logged_in_user_error):
+            print("Oculus Platform logged in user error: ", logged_in_user_error)
+        )
+    )\
+    .error(func(initialization_err):
+        print("Oculus Platform initialization error: ", initialization_err)
     )
