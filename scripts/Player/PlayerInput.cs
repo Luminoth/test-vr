cb3e962812ce6dc35f4bd3935c1f3d8bc9920418
Shane Lillie <ignignokterr@gmail.com>
2024-09-16 20:32:20 -0700

    Need some test input

--- scripts/Player/PlayerInput.cs
+++ scripts/Player/PlayerInput.cs
@@ -2,4 +2,23 @@ namespace VrTest.Player;
 
 public partial class PlayerInput : Node
 {
+    [Export]
+    private Vector2 _moveDirection;
+
+    public Vector2 MoveDirection => _moveDirection;
+
+    [Export]
+    private Vector2 _lookDirection;
+
+    public Vector2 LookDirection => _lookDirection;
+
+    #region Godot Lifecycle
+
+    public override void _Process(double delta)
+    {
+        _moveDirection = Input.GetVector("move left", "move right", "move forward", "move back");
+        _lookDirection = Input.GetVector("look left", "look right", "look forward", "look back");
+    }
+
+    #endregion
 }
