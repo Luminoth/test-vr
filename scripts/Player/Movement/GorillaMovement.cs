a0c5092f2fee9338015528f5e6779a57fe9de17a
Shane Lillie <ignignokterr@gmail.com>
2024-09-24 15:20:44 -0700

    Ok, but can we do Gorilla Tag movement ?

---
+++ scripts/Player/Movement/GorillaMovement.cs
@@ -0,0 +1,72 @@
+using VrTest.Managers;
+
+namespace VrTest.Player;
+
+// movement scripts need to be above the player character script
+// in tree order so that they execute first
+public partial class GorillaMovement : Node
+{
+    [Export]
+    private bool _enabled = true;
+
+    public bool Enabled
+    {
+        get => _enabled;
+        set
+        {
+            SetProcess(_enabled);
+            SetPhysicsProcess(_enabled);
+        }
+    }
+
+    [Export]
+    private XrPlayerCharacter _character;
+
+    private double _gravity;
+
+    #region Godot Lifecycle
+
+    public override void _Ready()
+    {
+        _gravity = ProjectSettings.GetSetting("physics/3d/default_gravity").AsDouble();
+
+        SetProcess(Enabled);
+        SetPhysicsProcess(Enabled);
+    }
+
+    public override void _PhysicsProcess(double delta)
+    {
+        if(!XrManager.Instance.IsXrInitialized) {
+            return;
+        }
+
+        // rotation in the physics step
+        // because hands will move from this
+        ApplyRotation((float)delta);
+
+        ApplyMovement((float)delta);
+    }
+
+    #endregion
+
+    private void ApplyRotation(float delta)
+    {
+        // match the character Y rotation to the camera
+        _character.GlobalRotation = _character.GlobalRotation with { Y = _character.Player.Camera.GlobalRotation.Y };
+    }
+
+    private void ApplyMovement(float delta)
+    {
+        // attempt to move the character to be under the camera on the X/Z plane
+        var currentPosition = _character.GlobalPosition with { Y = 0.0f };
+        var desiredPosition = _character.Player.Camera.GlobalPosition with { Y = 0.0f };
+        var currentVelocity = _character.Velocity;
+        _character.Velocity = (desiredPosition - currentPosition) / delta;
+        _character.MoveAndSlide();
+        _character.Velocity = currentVelocity;
+
+        // move the origin back to match if we didn't make it
+        var remaining = desiredPosition - _character.GlobalPosition with { Y = 0.0f };
+        _character.Player.GlobalPosition -= remaining;
+    }
+}
